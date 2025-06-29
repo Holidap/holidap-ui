8ed344528add78c8cc131d5a9c017371fd558250
Youngjoon Park <pyj2025@gmail.com>
2025-06-28 20:33:27 -0700

    add md files

--- src/data/GuideData.ts
+++ src/data/GuideData.ts
@@ -17,4 +17,32 @@ export const GuideData: GuideItemType[] = [
     tags: "항공",
     filePath: "/content/canada-immigration-packing-guide.md",
   },
+  {
+    id: "basic-documents",
+    title: "기본준비물 (Document)",
+    category: "한국",
+    tags: "서류",
+    filePath: "/content/basic-documents.md",
+  },
+  {
+    id: "one-way-ticket-booking",
+    title: "비행기 편도 예매",
+    category: "한국",
+    tags: "항공",
+    filePath: "/content/one-way-ticket-booking.md",
+  },
+  {
+    id: "job-platforms-in-canada",
+    title: "현지 잡 구하는 플랫폼 소개",
+    category: "캐나다",
+    tags: "취업",
+    filePath: "/content/job-platforms-in-canada.md",
+  },
+  {
+    id: "useful-apps-in-canada",
+    title: "캐나다 유용한 앱 추천",
+    category: "캐나다",
+    tags: "생활",
+    filePath: "/content/useful-apps-in-canada.md",
+  },
 ];
